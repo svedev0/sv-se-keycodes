9e7571774f75dfca939dc50828c0ed77a135579f
svedev <emanuel@kulich.me>
2023-11-13 19:03:02 +0100

    Create keycodes.h

---
+++ keycodes.h
@@ -0,0 +1,45 @@
+#define CKEY_SLASH     0x54
+#define CKEY_ASTERISK  0x55
+#define CKEY_DASH      0x56
+#define CKEY_PLUS      0x57
+
+#define CKEY_A         0x04
+#define CKEY_B         0x05
+#define CKEY_C         0x06
+#define CKEY_D         0x07
+#define CKEY_E         0x08
+#define CKEY_F         0x09
+#define CKEY_G         0x0A
+#define CKEY_H         0x0B
+#define CKEY_I         0x0C
+#define CKEY_J         0x0D
+#define CKEY_K         0x0E
+#define CKEY_L         0x0F
+#define CKEY_M         0x10
+#define CKEY_N         0x11
+#define CKEY_O         0x12
+#define CKEY_P         0x13
+#define CKEY_Q         0x14
+#define CKEY_R         0x15
+#define CKEY_S         0x16
+#define CKEY_T         0x17
+#define CKEY_U         0x18
+#define CKEY_V         0x19
+#define CKEY_W         0x1A
+#define CKEY_X         0x1B
+#define CKEY_Y         0x1C
+#define CKEY_Z         0x1D
+#define CKEY_AO        0x2F // Å
+#define CKEY_AE        0x34 // Ä
+#define CKEY_OE        0x33 // Ö
+
+#define CKEY_1         0x1E
+#define CKEY_2         0x1F
+#define CKEY_3         0x20
+#define CKEY_4         0x21
+#define CKEY_5         0x22
+#define CKEY_6         0x23
+#define CKEY_7         0x24
+#define CKEY_8         0x25
+#define CKEY_9         0x26
+#define CKEY_0         0x27
